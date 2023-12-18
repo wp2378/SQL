e55dbc05c2927046dea6d1cde15441b9fa082d3d
wp2378 <148727372+wp2378@users.noreply.github.com>
2023-12-18 17:52:05 +0900

    Add files via upload

---
+++ WITH절 실행문.sql
@@ -0,0 +1,30 @@
+-- HTA 사용자에게 객체 권한 부여하기
+GRANT SELECT
+ON EMPLOYEES
+TO HTA;
+
+-- HTA 사용자에게 부여된 객체 권한 회수하기
+REVOKE SELECT
+ON EMLOYEES
+FROM HTA;
+
+-- 특정 사용자에게 여러 개의 객체 권한을 부여하기
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON EMPLOYEES
+TO HTA;
+
+-- 특정 사용자에게 EMPLOYEES객체에 대한 모든 객체권한을 부여하기
+GRANT ALL PRIVILEGES
+ON EMPLOYEES
+TO HTA;
+
+-- 모든 사용자에게 EMPLOYEES객체에 대한 객체 권한을 부여하기
+GRANT SELECT
+ON EMPLOYEES
+TO PUBLIC;
+
+-- WITH GRANT OPTION을 사용하면 SELECT 권한을 부여받은 HTA가 다른 사용자에게 SELECT 권한을 부여할 수 있다.
+GRANT SELECT
+ON EMPLOYEES
+TO HTA
+WITH GRANT OPTION;
